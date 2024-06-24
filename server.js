aeb931dffa76ba0927107b3c3b66f151a78b93f0
ngarcia <garcia.noav@gmail.com>
2024-06-24 12:37:26 -1000

    feat: adding sockets, work on cleint

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from "express";
+import { Server } from "socket.io";
 import * as path from "path";
-import * as http from "http";
+import { createServer } from "http";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-const http_server = http.Server(app);
 const port = 8080;
 
 app.use(express.static(path.join(process.cwd(), "views/")));
+
 
 app.get("/", (request, response) => {
     response.sendFile(".", "index.html");
 });
 
-http_server.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("A user connected to the server!");
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected from the server!");
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
 });
